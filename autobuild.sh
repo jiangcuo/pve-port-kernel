d0aee5c489ad0c326bf84f704e092ae02505af6e
Jiangcuo <jiangcuo@lierfang.com>
2025-01-22 03:10:04 +0000

    refresh patches

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-git submodule update --init --depth=1
-debian/rules debian/control
-apt install devscripts  rsync -y
-yes |mk-build-deps --install --remove
 rm build -rf
 mkdir build && rsync -ra * .git build/
 cd build
