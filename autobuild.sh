f16fc3c194a3c0f2ca8c9594038b64f151632c26
Lierfang Support Team <itsupport@lierfang.com>
2025-08-07 14:43:12 +0000

    Add cross build

--- autobuild.sh
+++ autobuild.sh
@@ -1,9 +1,83 @@
 #!/bin/bash
-git submodule update --init --depth=1
-debian/rules debian/control
-apt install devscripts  rsync -y
-yes |mk-build-deps --install --remove
-rm build -rf
-mkdir build && rsync -ra * .git build/
-cd build
-dpkg-buildpackage -b -us -uc
+hostarch=host
+shell_path=$(realpath $(dirname $0))
+
+os_check(){
+  if [ ! -f /etc/os-release ]; then
+    echo "not debian"
+    exit 1
+  fi
+  source /etc/os-release
+  if [ "$VERSION_ID" != "13" ]; then
+    echo "only debian 13 is supported"
+    exit 1
+  fi
+}
+
+build(){
+    cd $shell_path
+    debian/rules debian/control 
+    apt update && apt install qemu-user-static python-dev-is-python3 rsync cross-config  -y
+
+    rsync -ra scripts linux  debian zfs  build
+    apt install asciidoc flex bc bison cpio dwarves flex kmod lz4 quilt xmlto zstd qemu-user-static -y
+    apt install -y libbabeltrace-dev:$hostarch \
+        libcap-dev:$hostarch \
+        libdw-dev:$hostarch \
+        libelf-dev:$hostarch \
+        libiberty-dev:$hostarch \
+        libunwind-dev:$hostarch \
+        libpfm4-dev:$hostarch \
+        libtraceevent-dev:$hostarch \
+        libnuma-dev:$hostarch \
+        libslang2-dev:$hostarch \
+        libssl-dev:$hostarch \
+        libzstd-dev:$hostarch \
+        libperl-dev:$hostarch \
+        systemtap-sdt-dev:$hostarch \
+        pkg-config:$hostarch \
+        crossbuild-essential-$hostarch \
+        libstdc++6:$hostarch
+
+    cd build
+    if [ "$hostarch" == "host" ]; then
+      dpkg-buildpackage -us -uc -b 
+    else
+      dpkg-buildpackage -us -uc -b -a$hostarch -d
+    fi
+
+}
+
+#os_check
+case $1 in
+    clean)
+    rm build *.deb pve-kernel* -rf
+    exit 0
+    ;;
+
+    aarch64)
+    hostarch=arm64
+    ;;
+
+    riscv64)
+    hostarch=riscv64
+
+    ;;
+
+    loongarch64)
+    hostarch=loong64
+
+    ;;
+    x86_64)
+    hostarch=amd64
+    ;;
+    help)
+    echo "usage: $0 [clean|arm64|riscv64|loongarch64|x86_64]"
+    exit 1
+    ;;
+    *)
+    ;;
+esac
+
+
+build
